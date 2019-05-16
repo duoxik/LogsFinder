932db1b59ba412f0ed3b1080366fe39d5a245a68
Vladislav Moiseev <duoxik@gmail.com>
2019-05-16 21:24:29 +0300

    MenuHelper created.

---
+++ src/com/duoxik/logfinder/view/MenuHelper.java
@@ -0,0 +1,42 @@
+package com.duoxik.logfinder.view;
+
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class MenuHelper {
+
+    public static JMenuItem addMenuItem(JMenu parent, String text, ActionListener actionListener) {
+        JMenuItem menuItem = new JMenuItem(text);
+        menuItem.addActionListener(actionListener);
+        parent.add(menuItem);
+        return menuItem;
+    }
+
+    public static JMenuItem addMenuItem(JMenu parent, String text, Action action) {
+        JMenuItem menuItem = addMenuItem(parent, action);
+        menuItem.setText(text);
+        return menuItem;
+    }
+
+    public static JMenuItem addMenuItem(JMenu parent, Action action) {
+        JMenuItem menuItem = new JMenuItem(action);
+        parent.add(menuItem);
+        return menuItem;
+    }
+
+    public static void initFileMenu(View view, JMenuBar menuBar) {
+        JMenu fileMenu = new JMenu("File");
+        menuBar.add(fileMenu);
+
+        addMenuItem(fileMenu, "Open", view);
+        fileMenu.addSeparator();
+        addMenuItem(fileMenu, "Exit", view);
+    }
+
+    public static void initHelpMenu(View view, JMenuBar menuBar) {
+        JMenu helpMenu = new JMenu("Help");
+        menuBar.add(helpMenu);
+
+        addMenuItem(helpMenu, "About", view);
+    }
+}
