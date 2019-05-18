6e61d746de1dcddbd9ecef9223515d13d2ecb66b
Vladislav Moiseev <duoxik@gmail.com>
2019-05-18 20:10:21 +0300

    Added new listener.

---
+++ src/com/duoxik/logfinder/gui/listeners/FileTreeListener.java
@@ -0,0 +1,28 @@
+package com.duoxik.logfinder.gui.listeners;
+
+import com.duoxik.logfinder.gui.FileTreeJPanel;
+import com.duoxik.logfinder.gui.Helper;
+
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.TreePath;
+import java.io.File;
+
+public class FileTreeListener implements TreeSelectionListener {
+
+    private FileTreeJPanel fileTree;
+
+    public FileTreeListener(FileTreeJPanel fileTree) {
+        this.fileTree = fileTree;
+    }
+
+    @Override
+    public void valueChanged(TreeSelectionEvent e) {
+        TreePath treePath = e.getPath();
+        String root = fileTree.getDirectory().getParent();
+        File selectedFile = Helper.transformToFilePath(root, treePath);
+        if (!selectedFile.isDirectory()) {
+            fileTree.readFile(selectedFile);
+        }
+    }
+}
