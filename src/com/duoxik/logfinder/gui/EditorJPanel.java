c2826c0e9c83f329865be9f57c98fb3a9e4f654e
Vladislav Moiseev <duoxik@gmail.com>
2019-05-18 21:01:05 +0300

    Editor pain changed. (Changed type of pane)

--- src/com/duoxik/logfinder/gui/EditorJPanel.java
+++ src/com/duoxik/logfinder/gui/EditorJPanel.java
@@ -2,22 +2,26 @@ package com.duoxik.logfinder.gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
 
 public class EditorJPanel extends JPanel {
 
-    private JEditorPane plainTextPane = new JEditorPane();
+    private JTextPane plainTextPane = new JTextPane();
     private JScrollPane plainScrollPane = new JScrollPane(plainTextPane);
 
     private View view;
+    private File file;
 
-    public EditorJPanel(String text, View view) {
+    public EditorJPanel(View view, File file, String text) {
         this.view = view;
-        plainTextPane.setText(text);
-        plainTextPane.setEditable(false);
-        init();
+        this.file = file;
+        init(text);
     }
 
-    private void init() {
+    private void init(String text) {
+        //plainTextPane.setContentType("text/html");
+        plainTextPane.setText(text);
+        plainTextPane.setEditable(false);
         setLayout(new BorderLayout());
         add(BorderLayout.CENTER, plainScrollPane);
     }
