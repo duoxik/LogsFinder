c256e945e2c571e2f145917220d5cc1b75762ec5
Vladislav Moiseev <duoxik@gmail.com>
2019-05-19 20:43:29 +0300

    Refactoring.

--- src/com/duoxik/logfinder/gui/OpenJFrame.java
+++ src/com/duoxik/logfinder/gui/OpenJFrame.java
@@ -1,15 +1,21 @@
 package com.duoxik.logfinder.gui;
 
-import com.duoxik.logfinder.gui.listeners.CancelButtonListener;
-import com.duoxik.logfinder.gui.listeners.FindButtonListener;
-
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class OpenJFrame extends JFrame {
+public class OpenJFrame extends JFrame implements ActionListener {
     private JTextField path = new JTextField(21);
     private JTextField type = new JTextField(10);
     private JTextField text = new JTextField(32);
+
+    private JLabel pathLabel = new JLabel("Directory path:");
+    private JLabel typeLabel = new JLabel("File type:");
+    private JLabel textLabel = new JLabel("Text:");
+
+    private JButton find = new JButton("Find");
+    private JButton cancel = new JButton("Cancel");
 
     private final View view;
 
@@ -19,35 +25,80 @@ public class OpenJFrame extends JFrame {
         init();
     }
 
-    public void init() {
+    public void lockFindButton() {
+        find.setEnabled(false);
+    }
+
+    public void unlockFindButton() {
+        find.setEnabled(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        switch (e.getActionCommand()) {
+            case "Find":
+                String path = this.path.getText().trim();
+                String type = this.type.getText().trim();
+                String text = this.text.getText().trim();
+
+                if (!path.isEmpty() && !type.isEmpty() && !text.isEmpty()) {
+                    setVisible(false);
+                    view.findLogs(path, type, text);
+                } else {
+                    setVisible(false);
+                    JOptionPane.showMessageDialog(
+                            null,
+                            "Data entered incorrectly. Try again ...",
+                            "Error",
+                            JOptionPane.ERROR_MESSAGE);
+                    setVisible(true);
+                }
+                break;
+
+            case "Cancel":
+                setVisible(false);
+        }
+    }
+
+    private void init() {
         setSize(375, 185);
         setLocationRelativeTo(null);
         setResizable(false);
 
-//        path.setText("Enter directory path...");
+        initTextFields();
+        initLabels();
+        initButtons();
+        initGrid();
+
+        pack();
+    }
+
+    private void initTextFields() {
         path.setText("/home/duoxik/IdeaProjects/LogsFinder/test/com/duoxik/logfinder/files");
         type.setText("log");
-//        text.setText("Enter text...");
         text.setText("client");
+//        path.setText("Enter directory path...");
+//        type.setText("log");
+//        text.setText("Enter text...");
+    }
 
-        JLabel pathLabel = new JLabel("Directory path:");
-        JLabel typeLabel = new JLabel("File type:");
-        JLabel textLabel = new JLabel("Text:");
-
+    private void initLabels() {
         pathLabel.setFont(new Font(Font.SANS_SERIF, Font.ITALIC, 12));
         typeLabel.setFont(new Font(Font.SANS_SERIF, Font.ITALIC, 12));
         textLabel.setFont(new Font(Font.SANS_SERIF, Font.ITALIC, 12));
+    }
 
-        JButton find = new JButton("Find");
+    private void initButtons() {
         find.setPreferredSize(new Dimension(75, 25));
         find.setFont(new Font(Font.SANS_SERIF, 0, 11));
-        find.addActionListener(new FindButtonListener(this));
+        find.addActionListener(this);
 
-        JButton cancel = new JButton("Cancel");
         cancel.setPreferredSize(new Dimension(75, 25));
         cancel.setFont(new Font(Font.SANS_SERIF, 0, 11));
-        cancel.addActionListener(new CancelButtonListener(this));
+        cancel.addActionListener(this);
+    }
 
+    private void initGrid() {
         final JPanel contentCenter = new JPanel(new GridBagLayout());
         contentCenter.add(pathLabel, new GridBagConstraints(0, 0, 1, 1, 0, 0, GridBagConstraints.WEST,
                 GridBagConstraints.NONE, new Insets(12, 10, 3, 0), 0, 0));
@@ -68,18 +119,5 @@ public class OpenJFrame extends JFrame {
 
         getContentPane().add(contentCenter, BorderLayout.CENTER);
         contentCenter.setBackground(new Color(239, 235, 231));
-        pack();
-    }
-
-    public boolean validateFields() {
-        return     !path.getText().trim().isEmpty()
-                && !type.getText().trim().isEmpty()
-                && !text.getText().trim().isEmpty();
-    }
-
-    public void findLogs() {
-        view.findLogs(path.getText().trim(),
-                      type.getText().trim(),
-                      text.getText().trim());
     }
 }
