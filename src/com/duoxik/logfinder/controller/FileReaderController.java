8d7aae2ff7c85bc2a2ee6bf6ffefbd88495d67bf
Vladislav Moiseev <duoxik@gmail.com>
2019-05-21 00:22:04 +0300

    Refactoring.

--- src/com/duoxik/logfinder/controller/FileReaderController.java
+++ src/com/duoxik/logfinder/controller/FileReaderController.java
@@ -8,49 +8,31 @@ public class FileReaderController {
 
     private static final int PAGE_SIZE = 1024 * 1024;
 
-    public String readFile(LogFile log, int pageNumber) {
-        String resultPage = null;
-
+    public String readFile(LogFile log, int pageNumber) throws IOException {
         try (
                 RandomAccessFile raf = new RandomAccessFile(log.getFile(), "r")
         ) {
             long position = PAGE_SIZE * (pageNumber - 1);
             raf.seek(position);
 
-            byte[] arr;
-            if (raf.length() - position < PAGE_SIZE) {
-                arr = new byte[(int) (raf.length() - position)];
-            } else {
-                arr = new byte[PAGE_SIZE];
-            }
+            byte[] arr = raf.length() - position < PAGE_SIZE
+                    ? new byte[(int) (raf.length() - position)]
+                    : new byte[PAGE_SIZE];
 
             raf.read(arr);
 
-            resultPage = new String(arr);
-        } catch (IOException e) {
-            e.printStackTrace();
+            return new String(arr);
         }
-
-        return resultPage;
     }
 
-    public int countPages(LogFile log) {
-        int countPages = -1;
-
+    public int countPages(LogFile log) throws IOException {
         try (
                 FileInputStream fis = new FileInputStream(log.getFile())
         ) {
-            if (fis.available() % (PAGE_SIZE) != 0)
-                countPages = fis.available() / (PAGE_SIZE) + 1;
-            else
-                countPages = fis.available() / (PAGE_SIZE);
+            return fis.available() % PAGE_SIZE != 0
+                    ? fis.available() / PAGE_SIZE + 1
+                    : fis.available() / PAGE_SIZE;
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-
-        return countPages;
     }
 }
